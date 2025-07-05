80620801fbdab24362a74899ddb8589ad54ceba7
stark <danystarrkk@gmail.com>
2025-07-05 17:43:39 -0500

    Entorno Kali Linux

---
+++ configs/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    lazy = false,
+    opts = {
+      style = "darker",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
