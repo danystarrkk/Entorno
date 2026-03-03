ae3429f87e2498aad43f5f9bff8421cd1de1fb34
Stark <45736989+danystarrkk@users.noreply.github.com>
2026-03-02 21:36:40 -0500

    Refactor postInstall.sh for improved clarity and structure

--- postInstall.sh
+++ postInstall.sh
@@ -16,7 +16,7 @@ grayColour="\e[0;37m\033[1m"
 ########## Salir ########
 
 function ctrl_c() {
-  echo -e ""
+  echo -e ""
 }
 
 trap ctrl_c SIGINT
@@ -35,135 +35,135 @@ rutaE="$HOME/Entorno/"
 
 function installDependencias() {
 
-  clear
+  clear
 
-  echo -e "\n\t ${blueColour} Instalación del Entorno \n\n${endColour}"
+  echo -e "\n\t ${blueColour} Instalación del Entorno \n\n${endColour}"
 
-  echo -en "${turquoiseColour}[1] Instalar dependencias [y/n]: ${endColour}" && read opt1
+  echo -en "${turquoiseColour}[1] Instalar dependencias [y/n]: ${endColour}" && read opt1
 
-  if [ "$opt1" == "y" ]; then
-    echo -e "\n${purpleColour}    [+] Instalando Dependencias......${endColour}"
+  if [ $opt1 == "y" ]; then
+    echo -e "\n${purpleColour}    [+] Instalando Dependencias......${endColour}"
 
-    sudo pacman -Syu
+    sudo pacman -Syu
 
-    sudo pacman -S --needed base-devel git
+    sudo pacman -S --needed --noconfirm base-devel git
 
-    echo -e "${blueColour}[+] Instalando repositorio BlackArch...${endColour}"
+    echo -e "${blueColour}[+] Instalando repositorio BlackArch...${endColour}"
 
-    curl -O https://blackarch.org/strap.sh
-    chmod +x strap.sh
-    sudo ./strap.sh
-    rm -f strap.sh
+    curl -O https://blackarch.org/strap.sh
+    chmod +x strap.sh
+    sudo ./strap.sh
+    rm -f strap.sh
 
-    sudo pacman -Syu
+    sudo pacman -Syu
 
-    sudo pacman -S --needed --noconfirm nmap qemu-guest-agent spice-vdagent whatweb arp-scan gobuster ffuf wfuzz burpsuite curl wget netcat openssh python ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-ubuntu-font-family ttf-opensans ttf-roboto adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts xdg-user-dirs
+    sudo pacman -S --noconfirm --needed nmap whatweb arp-scan gobuster ffuf wfuzz burpsuite curl wget netcat openssh python ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-ubuntu-font-family ttf-opensans ttf-roboto adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts xdg-user-dirs
 
-    echo -e "${greenColour}[+] Repositorio BlackArch instalado correctamente${endColour}"
+    echo -e "${greenColour}[+] Repositorio BlackArch instalado correctamente${endColour}"
 
-    echo -e "${blueColour}[+] Instalando yay ${endColour}\n"
+    echo -e "${blueColour}[+] Instalando yay ${endColour}\n"
 
-    git clone https://aur.archlinux.org/yay.git
+    git clone https://aur.archlinux.org/yay.git
 
-    mv yay configs
-    cd $rutaT/yay
-    makepkg -si
-    cd $rutaE
+    mv yay configs
+    cd $rutaT/yay
+    makepkg -si
+    cd $rutaE
 
-    yay -S --noconfirm net-tools flameshot pocl xclip xsel neovim xorg-xsetroot git vim zsh bspwm sxhkd picom polybar rofi feh kitty zsh-syntax-highlighting bat lsd npm open-vm-tools wmname dash glib2-devel gtkmm3 firefox docker docker-compose unzip sddm wget curl arandr nitrogen firefox less tree ripgrep
+    yay -S --noconfirm qemu-guest-agent spice-vdagent net-tools flameshot pocl xclip xsel neovim xorg-xsetroot git vim zsh bspwm sxhkd picom polybar rofi feh kitty zsh-syntax-highlighting bat lsd npm open-vm-tools wmname dash glib2-devel gtkmm3 firefox docker docker-compose unzip sddm wget curl arandr nitrogen firefox less tree ripgrep
 
-    if [ $(echo $?) -eq 0 ]; then
-      echo -e "${greenColour}    [+] Instalación de dependecias correctamente.....${endColour}"
-    else
-      echo -e "${redColour}    [!] Error en la Instalación de Dependencias....${endColour}"
-    fi
+    if [ $(echo $?) -eq 0 ]; then
+      echo -e "${greenColour}    [+] Instalación de dependecias correctamente.....${endColour}"
+    else
+      echo -e "${redColour}    [!] Error en la Instalación de Dependencias....${endColour}"
+    fi
 
-  else
-    echo -e "\n\t${redColour}[!] No se instalaran las dependecias, no se recomienda omitir este paso...  ${endColour}"
-  fi
+  else
+    echo -e "\n\t${redColour}[!] No se instalaran las dependecias, no se recomienda omitir este paso...  ${endColour}"
+  fi
 
 }
 
 function configuracionEntorno() {
 
-  echo -en "\n${blueColour}[2] Desea configurar el Entorno [y/n]:${endColour}" && read opt1
+  echo -en "\n${blueColour}[2] Desea configurar el Entorno [y/n]:${endColour}" && read opt1
 
-  if [ "$opt1" == "y" ]; then
+  if [ $opt1 == "y" ]; then
 
-    echo -e "\n${turquoiseColour}[+] Configuración del Entorno: ${endColour}"
+    echo -e "\n${turquoiseColour}[+] Configuración del Entorno: ${endColour}"
 
-  	xdg-user-dirs-update --force
-    wget -P $rutaT wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
-    unzip $rutaT/Hack.zip -d $rutaT/fonts
-    rm -rf $rutaT/Hack.zip
+  	xdg-user-dirs-update --force
+    wget -P $rutaT wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+    unzip $rutaT/Hack.zip -d $rutaT/fonts
+    rm -rf $rutaT/Hack.zip
 
-    mkdir $rutaP/.config/bin
-    touch $rutaP/.config/bin/target
+    mkdir $rutaP/.config/bin
+    touch $rutaP/.config/bin/target
 
-    cp -r $rutaT/bspwm $rutaP/.config
-    cp -r $rutaT/sxhkd $rutaP/.config
+    cp -r $rutaT/bspwm $rutaP/.config
+    cp -r $rutaT/sxhkd $rutaP/.config
 
-    chsh -s /bin/zsh
-    sudo chsh -s /bin/zsh
+    chsh -s /bin/zsh
+    sudo chsh -s /bin/zsh
 
-    chmod +x $HOME/.config/bspwm/scripts/*
+    chmod +x $HOME/.config/bspwm/scripts/*
 
-    cp -r $rutaT/wallpapers $rutaP/Pictures
-    cp -r $rutaT/nvim $rutaP/.config
+    cp -r $rutaT/wallpapers $rutaP/Pictures
+    cp -r $rutaT/nvim $rutaP/.config
 
-    cp -r $rutaT/kitty $rutaP/.config
-    sudo cp -r $rutaT/kitty /root/.config
+    cp -r $rutaT/kitty $rutaP/.config
+    sudo cp -r $rutaT/kitty /root/.config
 
-    sudo cp $rutaT/fonts/* /usr/share/fonts
+    sudo cp $rutaT/fonts/* /usr/share/fonts
 
-    cp -r $rutaT/picom $rutaP/.config
+    cp -r $rutaT/picom $rutaP/.config
 
-    sudo mkdir /usr/share/zsh-sudo/
+    sudo mkdir /usr/share/zsh-sudo/
 
-    sudo wget -O /usr/share/zsh-sudo/sudo.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh &>/dev/null
-    sudo systemctl enable vmtoolsd.service
-    sudo systemctl enable vmware-vmblock-fuse.service
+    sudo wget -O /usr/share/zsh-sudo/sudo.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh &>/dev/null
+    sudo systemctl enable vmtoolsd.service
+    sudo systemctl enable vmware-vmblock-fuse.service
+    sudo systemctl enable docker.service
     sudo systemctl enable qemu-guest-agent
     sudo systemctl enable spice-vdagentd
-    sudo systemctl enable docker.service
 
-    cd
+    cd
 
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k &>/dev/null
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k &>/dev/null
 
-    rm -rf $rutaP/.p10k.zsh
-    rm -rf $rutaP/.zshrc
+    rm -rf $rutaP/.p10k.zsh
+    rm -rf $rutaP/.zshrc
 
-    cp $rutaT/files/.zshrc $HOME
-    cp $rutaT/files/.p10k.zsh $HOME
-    cp $rutaT/files/.gitconfig $HOME
+    cp $rutaT/files/.zshrc $HOME
+    cp $rutaT/files/.p10k.zsh $HOME
+    cp $rutaT/files/.gitconfig $HOME
 
-    sudo touch /root/.zshrc
+    sudo touch /root/.zshrc
 
-    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k &>/dev/null
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k &>/dev/null
 
-    sudo rm -rf /root/.p10k.zsh
-    sudo rm -rf /root/.zshrc
+    sudo rm -rf /root/.p10k.zsh
+    sudo rm -rf /root/.zshrc
 
-    sudo cp $rutaT/files_root/.zshrc /root
-    sudo cp $rutaT/files_root/.p10k.zsh /root
+    sudo cp $rutaT/files_root/.zshrc /root
+    sudo cp $rutaT/files_root/.p10k.zsh /root
 
-    sudo cp -r $rutaT/kitty /root/.config/
-    sudo cp -r $rutaT/nvim /root/.config/
+    sudo cp -r $rutaT/kitty /root/.config/
+    sudo cp -r $rutaT/nvim /root/.config/
 
-    sudo ln -s -f $rutaP/.zshrc /root/.zshrc
+    sudo ln -s -f $rutaP/.zshrc /root/.zshrc
 
-    rm -rf ~/.config/polybar/
-    cp -r $rutaT/polybar $HOME/.config
-    cp -r $rutaT/rofi $rutaP/.config
+    rm -rf ~/.config/polybar/
+    cp -r $rutaT/polybar $HOME/.config
+    cp -r $rutaT/rofi $rutaP/.config
 
-    echo -e "${blueColour}[+] Instalando SDDM mínimo...${endColour}"
+    echo -e "${blueColour}[+] Instalando SDDM mínimo...${endColour}"
 
-    # Habilitar el servicio de SDDM
-    sudo systemctl enable sddm.service
+    # Habilitar el servicio de SDDM
+    sudo systemctl enable sddm.service
 
-    # Crear archivo de sesión para bspwm
-    sudo tee /usr/share/xsessions/bspwm.desktop >/dev/null <<'EOF'
+    # Crear archivo de sesión para bspwm
+    sudo tee /usr/share/xsessions/bspwm.desktop >/dev/null <<'EOF'
 [Desktop Entry]
 Name=bspwm
 Comment=Binary space partitioning window manager
@@ -171,26 +171,27 @@ Exec=bspwm
 Type=Application
 EOF
 
-    sudo pacman -Syu burpsuite whatweb seclists netcat
+    sudo pacman -Syu burpsuite whatweb seclists netcat
 
-    cd
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    cd
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
 
-    sudo cd
-    sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    sudo ~/.fzf/install
+    sudo cd
+    sudo git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    sudo ~/.fzf/install
 
-    if [ $(echo $?) -eq 0 ]; then
-      echo -e "\n${greenColour}[+] Se completo la configuración del Entorno.... ${endColour}"
-    else
-      echo -e "\n${redColour}[!] Error en la configuración del Entorno....${endColour}"
-    fi
-  else
-    echo -e "${redColour}\n\t[!] Configuración del Entorno Cancelado...\n\n${endColour}"
-  fi
+    if [ $(echo $?) -eq 0 ]; then
+      echo -e "\n${greenColour}[+] Se completo la configuración del Entorno.... ${endColour}"
+    else
+      echo -e "\n${redColour}[!] Error en la configuración del Entorno....${endColour}"
+    fi
+  else
+    echo -e "${redColour}\n\t[!] Configuración del Entorno Cancelado...\n\n${endColour}"
+  fi
 
 }
+
 function configurarHibernacion() {
   echo -e "\n${blueColour}[3] Configurando Swapfile de ${SWAP_SIZE_GB}GB para Hibernación...${endColour}"
 
