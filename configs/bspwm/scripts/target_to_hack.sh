2d470e03d887a1aa0916f817eac38e2993481766
Stark <45736989+danystarrkk@users.noreply.github.com>
2026-03-19 08:03:28 -0500

    Enhance target display formatting in target_to_hack.sh
    Updated output formatting for target display with larger icons.

--- configs/bspwm/scripts/target_to_hack.sh
+++ configs/bspwm/scripts/target_to_hack.sh
@@ -4,15 +4,17 @@ TARGET_FILE="$HOME/.config/bin/target"
 
 # Verificamos si el archivo existe y leemos su contenido de un solo golpe
 if [ -f "$TARGET_FILE" ]; then
-    read -r ip_target name_target < "$TARGET_FILE"
+  read -r ip_target name_target <"$TARGET_FILE"
 
-    if [ -n "$ip_target" ] && [ -n "$name_target" ]; then
-        echo "%{F#d11507}󰯐 %{F#ffffff}$ip_target - $name_target"
-    elif [ -n "$ip_target" ]; then
-        echo "%{F#d11507}󰯐 %{F#ffffff}$ip_target"
-    else
-        echo "%{F#d11507}󰓾 %{F#ffffff}No target"
-    fi
+  if [ -n "$ip_target" ] && [ -n "$name_target" ]; then
+    # Ícono grande con espacio para que no se corte
+    echo "%{F#d11507}%{T2}󰯐 %{T-}%{F#ffffff}$ip_target - $name_target"
+  elif [ -n "$ip_target" ]; then
+    echo "%{F#d11507}%{T2}󰯐 %{T-}%{F#ffffff}$ip_target"
+  else
+    # Ícono grande de "No target" con espacio
+    echo "%{F#d11507}%{T2}󰓾 %{T-}%{F#ffffff}No target"
+  fi
 else
-    echo "%{F#d11507}󰓾 %{F#ffffff}No target"
+  echo "%{F#d11507}%{T2}󰓾 %{T-}%{F#ffffff}No target"
 fi
