81a8d41502d4b2ac4742bbbd06bb051c59f6820d
stark <danystarrkk@gmail.com>
2026-06-27 19:58:54 -0500

    configuracion de .p10k corregida

--- configs/files_root/.p10k.zsh
+++ configs/files_root/.p10k.zsh
@@ -980,9 +980,9 @@
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='󱇐'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='󱇐'
   # Default context format (no privileges, no SSH): user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='󱇐'
 
   # Don't show context unless running with privileges or in SSH.
   # Tip: Remove the next line to always show context.
